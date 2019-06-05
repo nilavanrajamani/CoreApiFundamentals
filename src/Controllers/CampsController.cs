96ef5f950d86ee9e127798406af121e18a6e9176
Nilavan Rajamani <nilavanrajamani@gmail.com>
2019-06-05 23:11:23 +0530

    Initial Commit. Api basics

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper)
+        {
+            this.campRepository = campRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
+            try
+            {
+                var result = await campRepository.GetAllCampsAsync();
+                return mapper.Map<CampModel[]>(result);
+            }            
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure"); 
+            }
+        }
+    }
+}
---
+++ src/Data/CampModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampModel
+    {
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public Location Location { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
+    }
+}
